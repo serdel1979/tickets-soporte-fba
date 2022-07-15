5b30f88916e73da2ddf63b279d597d2bf431af0f
Sergio De Luca <sdlbsso@gmail.com>
2022-07-15 18:05:35 -0300

    crud de usuarios con roles y permisos

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -13,7 +13,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    console.log("payload --> ",payload);
     return { userId: payload.id, username: payload.user, roles: payload.roles }; //<-- el payload generado antes
   }
 }
