98aa2524989148d20234553f306b4f3ab06b570f
Sergio De Luca <sdlbsso@gmail.com>
2022-07-24 02:57:22 -0300

    reparacion de error en editado de usuario

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -3,11 +3,4 @@ import { Role } from 'src/common/role/role.enum';
 import { UserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(UserDto) {
-    
-        readonly  user: string;
-
-        readonly  password:string;
-
-        readonly  roles:Role[];
-      
 }
